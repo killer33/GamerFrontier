f6ca52aba877212f78accd1e54c1f43ae3ab81fc
killer33 <1336118586@qq.com>
2019-07-09 20:00:19 +0800

    v0.1.0

--- cloudfunctions/weather/index.js
+++ cloudfunctions/weather/index.js
@@ -5,9 +5,9 @@ cloud.init()
 var rp = require("request-promise");
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return rp(`http://api.tianapi.com/txapi/tianqi/?&key=1a102885a950cd8ec2aeb9355f7de0e0&city=上海`)
+  return rp(`http://api.tianapi.com/txapi/tianqi/?&key=1a102885a950cd8ec2aeb9355f7de0e0&ip=${event.city}`)
   .then( result => {
-    console.log(result);
+    console.log(rp(`http://api.tianapi.com/txapi/tianqi/?&key=1a102885a950cd8ec2aeb9355f7de0e0&city=${event.city}`));
     return result;
   }).catch(err => {
     console.log(err);
